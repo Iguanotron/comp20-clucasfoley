ad21d902237245b9bf38f8d99191c8303597d90d
Iguanotron <clucasfoley@gmail.com>
2016-10-16 13:19:29 -0400

    Map now finds geolocation data

--- mbta/app.js
+++ mbta/app.js
@@ -1,7 +1,35 @@
 var map;
-function render() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -34.397, lng: 150.644},
-		zoom: 8
-	});
+
+var myLat = 0;
+var myLng = 0;
+var me;
+
+var marker;
+var options = {
+	zoom: 9,
+	center: me,
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+function locateMe() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			myLat = position.coords.latitude;
+			myLng = position.coords.longitude;
+			me = new google.maps.LatLng(myLat, myLng);
+			map.panTo(me);
+			marker = new google.maps.Marker({
+				position: me,
+				title: "I am here"
+			});
+			marker.setMap(map);
+		});
+	} else {
+		alert('Geolocation not supported by your browser :\'(');
+	}
+}
+
+function init() {
+	map = new google.maps.Map(document.getElementById('map'), options);
+	locateMe();
 }
